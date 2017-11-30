a8eaa523f80e3dc1fa979f0745991b6ba5d2bdcf
Florent <florent.padiou@ercom.fr>
2017-11-30 15:45:19 +0100

    Add Config

---
+++ config/config.go
@@ -0,0 +1,87 @@
+package config
+
+import (
+	//"errors"
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"sync"
+	//"time"
+
+	yaml "gopkg.in/yaml.v2"
+
+	//"github.com/prometheus/common/config"
+)
+
+type Config struct {
+	Objects []Object `yaml:"object"`
+
+	// Catches all undefined fields and must be empty after parsing.
+	XXX map[string]interface{} `yaml:",inline"`
+}
+
+type SafeConfig struct {
+	sync.RWMutex
+	C *Config
+}
+
+func (sc *SafeConfig) ReloadConfig(confFile string) (err error) {
+	var c = &Config{}
+
+	yamlFile, err := ioutil.ReadFile(confFile)
+	if err != nil {
+		return fmt.Errorf("Error reading config file: %s", err)
+	}
+
+	if err := yaml.Unmarshal(yamlFile, c); err != nil {
+		return fmt.Errorf("Error parsing config file: %s", err)
+	}
+
+	sc.Lock()
+	sc.C = c
+	sc.Unlock()
+
+	return nil
+}
+
+type Object struct {
+	target string `yaml:"target"`
+	database string `yaml:"database"`
+	login string `yaml:"login"`
+	passwd string `yaml:"password"`
+
+	XXX map[string]interface{} `yaml:",inline"`
+}
+
+func checkOverflow(m map[string]interface{}, ctx string) error {
+	if len(m) > 0 {
+		var keys []string
+		for k := range m {
+			keys = append(keys, k)
+		}
+		return fmt.Errorf("unknown fields in %s: %s", ctx, strings.Join(keys, ", "))
+	}
+	return nil
+}
+
+func (s *Config) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	type plain Config
+	if err := unmarshal((*plain)(s)); err != nil {
+		return err
+	}
+	if err := checkOverflow(s.XXX, "config"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Object) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	type plain Object
+	if err := unmarshal((*plain)(s)); err != nil {
+		return err
+	}
+	if err := checkOverflow(s.XXX, "object"); err != nil {
+		return err
+	}
+	return nil
+}
